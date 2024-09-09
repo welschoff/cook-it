a7710e7f2827af453278d8203c68f16c6a429bf2
welschoff <h.welschoff@gmx.de>
2024-09-09 17:46:57 +0200

    Create Nutriton Component

---
+++ components/Nutritions.tsx
@@ -0,0 +1,51 @@
+import { CheckBox } from '@rneui/base';
+import { useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Collapsible } from './Collapsible';
+
+export default function Nutritions() {
+  const nutritions = ['Laktose', 'Gluten', 'Fruktose', 'Histamin', 'Sorbitin'];
+
+  const [checkedState, setCheckedState] = useState<Record<string, boolean>>(
+    nutritions.reduce((state, nutrition) => {
+      state[nutrition] = false;
+      return state;
+    }, {} as Record<string, boolean>)
+  );
+
+  const handleCheck = (nutrition: string) => {
+    setCheckedState((prevState) => ({
+      ...prevState,
+      [nutrition]: !prevState[nutrition],
+    }));
+  };
+  return (
+    <Collapsible title="Lebensmittelintoleranzen">
+      <View style={styles.view}>
+        {nutritions.map((nutrition) => (
+          <CheckBox
+            key={nutrition}
+            checked={checkedState[nutrition]}
+            onPress={() => handleCheck(nutrition)}
+            title={nutrition}
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            containerStyle={{ backgroundColor: 'transparent', margin: 0 }}
+            checkedColor="black"
+          />
+        ))}
+      </View>
+    </Collapsible>
+  );
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
