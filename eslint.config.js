be1ae69407e2cef061fa4d894ccf0d18ff053399
welschoff <h.welschoff@gmx.de>
2024-09-12 19:20:06 +0200

    eslint precommit hook

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,13 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { ignores: ["node_modules/*"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  { settings: { react: { version: "detect" } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  { rules: { "@typescript-eslint/no-require-imports": "off" } },
 ];
