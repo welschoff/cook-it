6cf4e834042ebea0abea3664c66d601a0b3c3119
welschoff <h.welschoff@gmx.de>
2024-09-12 13:05:52 +0200

    eslint config change

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,13 @@
-import eslintPluginPrettier from "eslint-plugin-prettier";
-import eslintPluginTypescript from "@typescript-eslint/eslint-plugin";
-import typescriptParser from "@typescript-eslint/parser";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
 
 export default [
-  {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-      parser: typescriptParser,
-    },
-    plugins: {
-      prettier: eslintPluginPrettier,
-      "@typescript-eslint": eslintPluginTypescript,
-    },
-    rules: {
-      "prettier/prettier": "error",
-      semi: ["warn", "always"],
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/no-unused-vars": ["error"],
-    },
-  },
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
